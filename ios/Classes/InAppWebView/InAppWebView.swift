6432d99e5f5f149a594e000daad8e398725c7471
신기열 <keykat@gsretail.com>
2024-04-03 14:21:57 +0900

    [feature] add isInspectable

--- ios/Classes/InAppWebView/InAppWebView.swift
+++ ios/Classes/InAppWebView/InAppWebView.swift
@@ -64,9 +64,7 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
         self.channel = channel
         self.contextMenu = contextMenu
         self.initialUserScripts = userScripts
-        if #available(iOS 16.4, *) {
-            self.isInspectable = true
-        }
+        self.isInspectable = true
         uiDelegate = self
         navigationDelegate = self
         scrollView.delegate = self
